e37a864bd3931b83097adad7dffe33ec90615776
Paulina Bednarz <pauli-2004@wp.pl>
2025-03-01 03:39:10 +0100

    zmiana 4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,5 +13,40 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("At least 5 students are required.");
+
+            var sortedGrades = Students.Select(s => s.AverageGrade).OrderByDescending(g => g).ToList();   //sorts grades in descending order
+
+            int value = (int)Math.Ceiling(Students.Count * 0.2);  //20% of students
+
+            if (sortedGrades.IndexOf(averageGrade) < value)
+            {
+                return 'A';
+            }
+            else if (sortedGrades.IndexOf(averageGrade) < value * 2)
+            {
+                return 'B';
+            }
+            else if (sortedGrades.IndexOf(averageGrade) < value * 3)
+            {
+                return 'C';
+            }
+            else if (sortedGrades.IndexOf(averageGrade) < value * 4)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+           
+        }
+
     }
+
 }
